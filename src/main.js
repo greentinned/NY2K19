010a5305c9d78ffb2b4c5d032eb58e00e53bd561
Sergey Korotkov <hello.to@me.com>
2018-11-14 20:50:41 +0300

    Init

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import 'p5'
+import { preload, setup, update, draw, windowResized } from './sketch'
+
+/**
+ * State
+ */
+
+let state = {}
+
+/**
+ * Lifecycle
+ */
+
+window.preload = preload
+
+window.setup = () => {
+  setup(state)
+}
+
+window.draw = () => {
+  update(state)
+  draw(state)
+}
+
+window.windowResized = () => {
+  windowResized(state)
+}
---
+++ src/sketch.js
@@ -0,0 +1,45 @@
+/**
+ * Global
+ */
+
+let font
+
+/**
+ * Lifecycle
+ */
+
+export function preload() {
+  font = loadFont('assets/Akrobat-Bold.otf')
+}
+
+export function setup(state) {
+  createCanvas(windowWidth, windowHeight, WEBGL)
+  state = {
+    fps: 0
+  }
+}
+
+export function update(state) {
+  state.fps = round(frameRate())
+}
+
+export function draw(state) {
+  background(50)
+  drawFPS(state)
+}
+
+export function windowResized(state) {
+  resizeCanvas(windowWidth, windowHeight)
+}
+
+/**
+ * User
+ */
+
+function drawFPS(state) {
+  fill(255)
+  textFont(font)
+  let x = -width / 2 + 10
+  let y = -height / 2 + 20
+  text(`FPS: ${state.fps}`, x, y)
+}
